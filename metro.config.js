6329474004f9b6f5386a8bfd4e47d852209035d3
lbqds <lbqds@outlook.com>
2024-06-24 15:52:06 +0800

    Using @alephium/wormhole-sdk 0.3.1-rc.2

--- metro.config.js
+++ metro.config.js
@@ -6,6 +6,13 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  resolver: {
+    extraNodeModules: {
+      stream: require.resolve('readable-stream'),
+      assert: require.resolve('assert')
+    }
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
